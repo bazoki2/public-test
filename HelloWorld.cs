a1a73225fec79301303f8be15aff09c3adc502dc
bazoki <32161145+bazoki@users.noreply.github.com>
2018-09-20 08:56:47 +0100

    asd

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+namespace HelloWorld
+{
+    public class Program
+    {
+        public void Main(string args[])
+        {
+            //TODO: 
+            //TOOD: AKIAIOSFODNN7EXAMPLE
+            //TODO2: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+        }
+    }
+}
