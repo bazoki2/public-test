286f05d189754bcd506d63fceff5b07e6aa7debd
bazoki <32161145+bazoki@users.noreply.github.com>
2018-09-24 08:54:43 +0100

    Test1 (#24)
    * DMARC-1 aloo
    
    * DMARC-1 test
    
    * DMARC-1 test

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //TODO: Test 12345678910
+            //TODO: Test 1234567891011
             //TOOD: AKIAIOSFODNN7EXAMPLE
             //TODO2: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 
